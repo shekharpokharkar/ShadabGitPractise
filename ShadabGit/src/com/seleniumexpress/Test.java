836a19d28e486d74d8f1933f6c74d3a019ba6208
shekharpokharkar <sheharpokharkar14@gmail.com>
2025-11-13 22:09:04 +0530

    All changes are comited to shekhar branch after pushing to remote branch

--- ShadabGit/src/com/seleniumexpress/Test.java
+++ ShadabGit/src/com/seleniumexpress/Test.java
@@ -3,7 +3,11 @@ package com.seleniumexpress;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("*******************");
+		System.out.println("Inside Shekhar Branch");
+		float fino1=200.00f;
+		double ino2=200.00;
+		long ino3=5263l;
+		System.out.println(fino1+ino2+ino3);
 	}
 
 }
