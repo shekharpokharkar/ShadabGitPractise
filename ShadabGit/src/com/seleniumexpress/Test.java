af0403d01054a1d2698327203ceeef958c050abb
shekharpokharkar <132981368+shekharpokharkar@users.noreply.github.com>
2025-11-13 23:04:49 +0530

    Now conflict issue are resolved

--- ShadabGit/src/com/seleniumexpress/Test.java
+++ ShadabGit/src/com/seleniumexpress/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 
-		System.out.println("*******************");
+		System.out.println("++++++++++++++++++++++++++++++++++++++++++++++++++++++");
 		int ino1 = 101;
 		int ino2 = 201;
 		System.out.println("Addition of two number is:" + (ino1 + ino2));
@@ -12,7 +12,7 @@ public class Test {
 		float fino1 = 200.00f;
 		long ino3 = 5263l;
 		System.out.println(fino1 + ino2 + ino3);
-
+		System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++");
 	}
 
 }
