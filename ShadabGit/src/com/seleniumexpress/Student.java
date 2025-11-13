91c6e58f6e13929909ee2d03b8f2186327bec89b
shekharpokharkar <sheharpokharkar14@gmail.com>
2025-11-13 21:58:18 +0530

    All Changed Commited in sagar 3

--- ShadabGit/src/com/seleniumexpress/Student.java
+++ ShadabGit/src/com/seleniumexpress/Student.java
@@ -2,4 +2,38 @@ package com.seleniumexpress;
 
 public class Student {
 
+	private String studentName;
+	private Long studentRollNo;
+	private String studetnCity;
+
+	public String getStudentName() {
+		return studentName;
+	}
+
+	public void setStudentName(String studentName) {
+		this.studentName = studentName;
+	}
+
+	public Long getStudentRollNo() {
+		return studentRollNo;
+	}
+
+	public void setStudentRollNo(Long studentRollNo) {
+		this.studentRollNo = studentRollNo;
+	}
+
+	public String getStudetnCity() {
+		return studetnCity;
+	}
+
+	public void setStudetnCity(String studetnCity) {
+		this.studetnCity = studetnCity;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [studentName=" + studentName + ", studentRollNo=" + studentRollNo + ", studetnCity="
+				+ studetnCity + "]";
+	}
+
 }
