8b92cc3bc4058a39109aec4891f222474a32c331
shekharpokharkar <sheharpokharkar14@gmail.com>
2025-11-13 21:54:24 +0530

    First Commit in sagar

--- ShadabGit/src/com/seleniumexpress/Test.java
+++ ShadabGit/src/com/seleniumexpress/Test.java
@@ -4,6 +4,9 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("*******************");
+		int ino1=101;
+		int ino2=201;
+		System.out.println("Addition of two number is:"+(ino1+ino2));
 	}
 
 }
